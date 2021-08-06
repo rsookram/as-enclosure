8bfa49a966cd4315f6604eac48388ec488c3840e
rsookram <rashadsookram@gmail.com>
2021-08-05 20:27:10 -0400

    Read data from stdin

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,67 @@
+import readline from "readline";
+
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 
-import { processDir } from "./process-dir.js"
 import { Tree } from "./Tree.tsx"
 
+const fromStdin = async () => {
+  const rl = readline.createInterface(process.stdin)
+
+  const tree = {
+    name: '.',
+    path: '',
+    size: 4096
+  }
+
+  for await (const line of rl) {
+    const [path, count] = line.split(':')
+    const segments = path.split('/')
+
+    let node = tree
+
+    let currentPath = ''
+
+    for (segment of segments.slice(0, segments.length - 1)) {
+      if (currentPath) {
+        currentPath += '/' + segment
+      } else {
+        currentPath += segment
+      }
+
+      node.children = node.children || []
+      const children = node.children
+
+      let foundNode = children.find(n => n.name === segment)
+      if (foundNode) {
+        node = foundNode
+      } else {
+        const newNode = {
+          name: segment,
+          path: currentPath,
+          size: 4096,
+        }
+        children.push(newNode)
+        node = newNode
+      }
+    }
+
+    const children = node.children || []
+    node.children = children
+
+    const name = segments[segments.length - 1]
+    children.push({
+      name,
+      path: currentPath + '/' + name,
+      size: parseInt(count, 10)
+    })
+  }
+
+  return tree
+}
+
 const main = async () => {
-  const data = await processDir(`./`);
+  const data = await fromStdin();
 
   const componentCodeString = ReactDOMServer.renderToStaticMarkup(
     <Tree data={data} />
