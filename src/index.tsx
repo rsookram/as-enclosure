40ff273778c3bb817da1d8e4c787223509539a3d
rsookram <rashadsookram@gmail.com>
2021-08-05 21:03:45 -0400

    Extract data processing to another file

--- src/index.tsx
+++ src/index.tsx
@@ -1,67 +1,11 @@
-import readline from "readline";
-
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 
+import { readFromStdin } from "./read-from-stdin.ts"
 import { Tree } from "./Tree.tsx"
 
-const fromStdin = async () => {
-  const rl = readline.createInterface(process.stdin)
-
-  const tree = {
-    name: '.',
-    path: '',
-    size: 4096
-  }
-
-  for await (const line of rl) {
-    const [path, count] = line.split(':')
-    const segments = path.split('/')
-
-    let node = tree
-
-    let currentPath = ''
-
-    for (segment of segments.slice(0, segments.length - 1)) {
-      if (currentPath) {
-        currentPath += '/' + segment
-      } else {
-        currentPath += segment
-      }
-
-      node.children = node.children || []
-      const children = node.children
-
-      let foundNode = children.find(n => n.name === segment)
-      if (foundNode) {
-        node = foundNode
-      } else {
-        const newNode = {
-          name: segment,
-          path: currentPath,
-          size: 4096,
-        }
-        children.push(newNode)
-        node = newNode
-      }
-    }
-
-    const children = node.children || []
-    node.children = children
-
-    const name = segments[segments.length - 1]
-    children.push({
-      name,
-      path: currentPath + '/' + name,
-      size: parseInt(count, 10)
-    })
-  }
-
-  return tree
-}
-
 const main = async () => {
-  const data = await fromStdin();
+  const data = await readFromStdin();
 
   const componentCodeString = ReactDOMServer.renderToStaticMarkup(
     <Tree data={data} />
---
+++ src/read-from-stdin.ts
@@ -0,0 +1,64 @@
+import readline from "readline"
+
+export const readFromStdin = async () => {
+  const rl = readline.createInterface(process.stdin)
+
+  const tree = {
+    name: '.',
+    path: '',
+    size: 4096
+  }
+
+  for await (const line of rl) {
+    updateTree(tree, parseLine(line))
+  }
+
+  return tree
+}
+
+const updateTree = (node, { dirs, name, count }) => {
+  let currentPath = ''
+
+  for (dir of dirs) {
+    if (currentPath) {
+      currentPath += '/' + dir
+    } else {
+      currentPath += dir
+    }
+
+    node.children = node.children || []
+    const children = node.children
+
+    let foundNode = children.find(n => n.name === dir)
+    if (foundNode) {
+      node = foundNode
+    } else {
+      const newNode = {
+        name: dir,
+        path: currentPath,
+        size: 4096,
+        children: []
+      }
+      children.push(newNode)
+      node = newNode
+    }
+  }
+
+  node.children.push({
+    name,
+    path: currentPath + '/' + name,
+    size: count
+  })
+}
+
+// Expected format for line: "src/index.tsx:4"
+const parseLine = (line) => {
+  const [path, count] = line.split(':')
+  const segments = path.split('/')
+
+  return {
+    dirs: segments.slice(0, segments.length - 1),
+    name: segments[segments.length - 1],
+    count: parseInt(count, 10)
+  }
+}
